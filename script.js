824e0725a4023c9c45a4b1c27d780b510b1a97b4
Антон Плетнёв <anton.pletnew@mail.ru>
2021-09-25 23:04:46 +0300

    Работа с todo элементами(зачеркивание, удаление), изменение стилей

--- script.js
+++ script.js
@@ -1,32 +1,113 @@
 
+    document.querySelector('input').addEventListener('keydown', function (e) {
+        if (e.keyCode === 13) {
+            listMaker();
+        }
 
-let arr = [];
-function createNote() {
-    let note = document.getElementById('inp').value;
-    if (note != '') {
-        arr.push(note);
-        console.log(note);
-        console.log(arr);
-        return note;
-    }
+    })
+
+function listMaker() {
+    let inp = document.getElementById('inp').value;
+    let todoList = document.getElementById('todo-list')
+    let todoContent = document.createElement('div');
+    todoContent.classList.add("todo");
+
+
+    const todoElement = document.createElement('li');
+    todoElement.classList.add('listElement');
+    todoElement.textContent = inp;
+    todoContent.appendChild(todoElement);
+
+
+    let buttonDelete = document.createElement('button');
+    buttonDelete.classList.add('btn-delete');
+    buttonDelete.textContent = '-';
+    todoContent.appendChild(buttonDelete);
+    todoList.appendChild(todoContent);
+    document.getElementById('inp').value = '';
+}
+
+todoList = document.getElementById('todo-list');
+if(todoList) {
+    todoList.addEventListener('click', function (event) {
+        console.log(event);
+        // console.log(event.target.tagName);
+        // console.log(event.target.tagName == 'LI');
+        if (event.target.tagName == 'LI') {
+            event.target.classList.toggle('done');
+        } else if(event.target.tagName == 'BUTTON') {
+            // alert('btnka');
+            let div = event.target.parentNode;
+            div.remove();
+        }
+    });
+
+
 }
 
 
-function listMaker(note) {
-    let ul = document.querySelector('ul');
-    let li = document.createElement('li');
-    li.innerHTML = note;
-    ul.appendChild(li);
-}
 
-function call() {
-    let noteFor = createNote()
-    // if (noteFor !=== 'undefined') {
-    //     listMaker(noteFor)
-    // }
-    listMaker(noteFor);
+    // let todoElement = document.createElement('li');
+    // todoElement.classList.add('listElement');
+    // todoElement.innerHTML = note;
+    // todoContent.appendChild(todoElement);
+    // let buttonDelete = document.createElement('button');
+    // buttonDelete.classList.add('btn-delete');
+    // buttonDelete.textContent = '-';
+    // todoContent.appendChild(buttonDelete);
+    // todoList.appendChild(todoContent);}
 
-}
+
+
+
+
+
+// код 23 сент
+
+//
+// let itemsArr = []; //сущ
+// function createNote() {
+//     let note = document.getElementById('inp').value; //содержит содержимое инпута
+//     itemsArr.push(note);
+//     localStorage.setItem('items', JSON.stringify(itemsArr)) //записываются в localstorage
+//     return note;
+//
+// }
+
+// console.log(itemsArr);     !!! реально выводит содержимое и если вывести массив то в нем всегда бует свежая коллекция инпутв
+
+
+
+// const todoList = document.querySelector('.todo-list')
+//
+// function listMaker(note) {
+//     const todoContent = document.createElement('div');
+//     todoContent.classList.add("todo");
+//     const todoElement = document.createElement('li');
+//     todoElement.classList.add('listElement');
+//     todoElement.innerHTML = note;
+//     todoContent.appendChild(todoElement);
+//     const buttonDelete = document.createElement('button');
+//     buttonDelete.classList.add('btn-delete');
+//     buttonDelete.textContent = '-';
+//     todoContent.appendChild(buttonDelete);
+//     todoList.appendChild(todoContent);}
+//
+//
+//
+// function call() {
+//     listMaker(createNote());
+//
+// }
+
+
+// код 23 сент зконч
+
+
+
+
+
+
 
 
 
